cefb4f631806f7833094056582c9dfe6d2d51c3f
Abhishek Roy <abhishekroy8487@gmail.com>
2023-02-18 11:58:52 +0530

    routes added and api to generate image done

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -4,9 +4,9 @@ const connectDB = (url) => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(url)
-    .then(() => console.log("connected to mongo"))
+    .then(() => console.log("connected to mongodb"))
     .catch((err) => {
-      console.error("failed to connect with mongo");
+      console.error("failed to connect with mongobd");
       console.error(err);
     });
 };
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./db/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js"
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello From Backend");
